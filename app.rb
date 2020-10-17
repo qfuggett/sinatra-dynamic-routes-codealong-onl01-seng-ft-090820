5e247fd82ea9cb0867035141f1e59536fc87211b
QueenTesa Fuggett <qfuggett@gmail.com>
2020-10-17 02:49:33 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -21,8 +21,6 @@ class App < Sinatra::Base
   end
   
   get "/multiply/:num1/:num2" do
-    @n1 = params[:num1].to_i
-    @n2 = params[:num2].to_i
-    @n1 * @n2
+    params[:num1].to_i * params[:num2].to_i
   end
 end
